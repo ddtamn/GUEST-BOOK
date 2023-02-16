eea6016826d607cf3f2f8fdc762f677c5bd0a1ca
ddtamn <disanderlidsbun@gmail.com>
2023-02-17 01:12:43 +0800

    facingMode "Environment"

--- src/routes/scanner/+page.svelte
+++ src/routes/scanner/+page.svelte
@@ -1,51 +1,51 @@
 <script>
 	import { Html5Qrcode } from 'html5-qrcode';
 	import { onMount } from 'svelte';
-	// This method will trigger user permissions
 
-	onMount(() => {
-		Html5Qrcode.getCameras()
-			.then((devices) => {
-				/**
-				 * devices would be an array of objects of type:
-				 * { id: "id", label: "label" }
-				 */
-				if (devices && devices.length) {
-					var cameraId = devices[0].id;
-					const html5Qrcode = new Html5Qrcode('reader', 'false');
-					html5Qrcode.start(
-						cameraId,
+
+
+	/**
+	 * @type {string | MediaTrackConstraints}
+	 */
+	 export let cameraId;
+
+
+	const startScan = () => {
+		const reader = new Html5Qrcode('reader', false);
+		reader.start({facingMode : "environment"},
 						{
 							fps: 10, // Optional frame per seconds for qr code scanning
 							qrbox: { width: 250, height: 250 } // Optional if you want bounded box UI
 						},
 						(decodedText, decodedResult) => {
 							// do something when code is read
-							if (decodedText) {
-								console.log(decodedText);
-								html5Qrcode.stop().then((ignore) => {
-									console.log(ignore);
-								}).catch((err) => {
-									console.log(err);
-								})
-
-							}
-
+							console.log(decodedText, decodedResult);
 						},
 						(errorMessage) => {
 							// parse error, ideally ignore it.
-							console.log(errorMessage);
-						}
-					);
-				}
-			})
-			.catch((err) => {
-				// handle err
-				console.log(err);
-			});
-	});
+							// console.log(errorMessage);
+						});
+	}
+
+	
+
+	onMount(() => {
+		Html5Qrcode.getCameras().then((devices) => {
+			if (devices && devices.length) {
+				cameraId = devices[0].id
+			}
+		})
+	}) 
 </script>
 
-<div class="flex justify-center items-center w-full h-[100vh]">
-	<div id="reader" class="w-full" />
+<div class="container">
+	<div id="reader" />
+	<button on:click={startScan}>Start Scan</button>
 </div>
+
+<style>
+	.container {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
